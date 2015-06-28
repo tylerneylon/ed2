22fcdb44c99bc7c9c0033a564f855e2b2d5aaed1
Tyler Neylon <tylerneylon@gmail.com>
2015-06-28 15:14:08 -0700

    Be able to load a file into a buffer.

--- ed2.c
+++ ed2.c
@@ -11,23 +11,65 @@
 
 // Standard includes.
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <sys/stat.h>
 
+
+///////////////////////////////////////////////////////////////////////////
 // Globals.
+///////////////////////////////////////////////////////////////////////////
 
-char filename[1024];
+char   filename[1024];
+char * buffer;
+size_t buffer_size;  // This excludes a final NULL terminator.
 
+
+///////////////////////////////////////////////////////////////////////////
+// Internal functions.
+///////////////////////////////////////////////////////////////////////////
+
+void load_filename() {
+  FILE *f = fopen(filename, "rb");
+  // TODO Handle the case that f is NULL.
+  struct stat file_stats;
+  int is_err = fstat(fileno(f), &file_stats);
+  // TODO Handle is_err != 0.
+
+  buffer_size = file_stats.st_size;
+  buffer = malloc(buffer_size + 1);  // + 1 for the final NULL character.
+
+  is_err = fread(buffer,       // buffer ptr
+                 1,            // item size
+                 buffer_size,  // num items
+                 f);           // stream
+  buffer[buffer_size] = '\0';  // Manually add a final NULL character.
+
+  // TODO Handle is_err != 0.
+
+  fclose(f);
+}
+
+
+///////////////////////////////////////////////////////////////////////////
+// Main.
+///////////////////////////////////////////////////////////////////////////
 
 int main(int argc, char **argv) {
 
   // Initialization.
+
+  buffer_size = 0;  // Indicates no buffer is loaded.
 
   if (argc < 2) {
     // The empty string indicates no filename has been given yet.
     filename[0] = '\0';
   } else {
     strcpy(filename, argv[1]);
-    // TODO Load the file.
+    load_filename();
+
+    // TEMP
+    printf("File contents:'''\n%s'''\n", buffer);
   }
 
   // Enter our read-eval-print loop (REPL).
