8548858f0476dcb433cd74822eb8544d8ae1c329
Tyler Neylon <tylerneylon@gmail.com>
2015-06-29 13:12:18 -0700

    Add the find_lines function.

--- ed2.c
+++ ed2.c
@@ -20,16 +20,47 @@
 // Globals.
 ///////////////////////////////////////////////////////////////////////////
 
+// The current filename.
 char   filename[1024];
+
+// The buffer that holds the text.
 char * buffer;
 size_t buffer_size;  // This excludes a final NULL terminator.
+
+// 0-indexed line n has its 1st character at line_indexes[n] and its last
+// at line_indexes[n + 1] - 1.
+// The last line begines at line_indexes[num_lines - 1].
+int *  line_indexes;
+int    num_lines;
 
 
 ///////////////////////////////////////////////////////////////////////////
 // Internal functions.
 ///////////////////////////////////////////////////////////////////////////
 
-void load_filename() {
+void find_lines() {
+  char *buf_copy = strdup(buffer);
+  char *buf_copy_ptr = buf_copy;
+  num_lines = 0;
+  char *line;
+
+  // TODO Correctly allocate line_indexes; maybe using cstructs is good.
+  line_indexes = malloc(128 * sizeof(int));
+
+  while ((line = strsep(&buf_copy_ptr, "\n"))) {
+    printf("line:\n%s\n", line);
+    line_indexes[num_lines++] = (line - buf_copy);
+    printf("%x\n", line_indexes[num_lines - 1]);
+  }
+  line_indexes[num_lines] = strlen(buffer);
+  printf("%x\n", line_indexes[num_lines]);
+  printf("num_lines=%d\n", num_lines);
+
+  free(buf_copy);
+}
+
+// Load the file at the global `filename` into the global `buffer`.
+void load_file() {
   FILE *f = fopen(filename, "rb");
   // TODO Handle the case that f is NULL.
   struct stat file_stats;
@@ -48,6 +79,8 @@ void load_filename() {
   // TODO Handle is_err != 0.
 
   fclose(f);
+
+  find_lines();
 }
 
 
@@ -66,7 +99,7 @@ int main(int argc, char **argv) {
     filename[0] = '\0';
   } else {
     strcpy(filename, argv[1]);
-    load_filename();
+    load_file();
 
     // TEMP
     printf("File contents:'''\n%s'''\n", buffer);
