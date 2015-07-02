64269cf0a6b473975ecbc7ccc7aec777f6c6b923
Tyler Neylon <tylerneylon@gmail.com>
2015-07-02 11:11:32 -0700

    Add current_line and a basic p(rint) command.

--- ed2.c
+++ ed2.c
@@ -31,6 +31,8 @@ char   filename[1024];
 // The byte stream can be formed by joining this array with "\n".
 Array  lines = NULL;
 
+int    current_line;
+
 
 ///////////////////////////////////////////////////////////////////////////
 // Internal functions.
@@ -46,6 +48,15 @@ void line_releaser(void *line_vp, void *context) {
 void init() {
   lines = array__new(64, sizeof(char *));
   lines->releaser = line_releaser;
+
+  // TODO This default only matters for new files. Study ed's behavior to
+  //      determine what the current line effectively is for new files.
+  //      This isn't obvious to me because I have the sense that 'a' will
+  //      append after line 0, while 'i' will insert before line 1.
+  // This variable is the same as what the user considers it; this is
+  // non-obvious in that our lines array is 0-indexed, so we have to be careful
+  // when indexing into `lines`.
+  current_line = 0;
 }
 
 // Separate a raw buffer into a sequence of indexed lines.
@@ -58,6 +69,13 @@ void find_lines(char *buffer) {
   char *line;
   while ((line = strsep(&buffer_ptr, "\n"))) {
     array__new_val(lines, char *) = strdup(line);
+  }
+
+  // Set the current line to the last, keeping in mind that a final empty string
+  // in `lines` indicates the buffer ends with a newline.
+  current_line = lines->count;
+  if (*array__item_val(lines, lines->count - 1, char *) == '\0') {
+    current_line--;
   }
 }
 
@@ -84,6 +102,17 @@ void load_file() {
 
   find_lines(buffer);
   free(buffer);
+  printf("%zd\n", buffer_size);  // Report how many bytes we read.
+}
+
+void run_command(char *command) {
+
+  if (strcmp(command, "q") == 0) exit(0);
+
+  if (strcmp(command, "p") == 0) {
+    // Print the current line.
+    printf("%s\n", array__item_val(lines, current_line - 1, char *));
+  }
 }
 
 
@@ -103,16 +132,18 @@ int main(int argc, char **argv) {
     strcpy(filename, argv[1]);
     load_file();
 
-    // TEMP
-    printf("File contents:'''\n");
-    array__for(char **, line, lines, i) printf("%s%s", (i ? "\n" : ""), *line);
-    printf("'''\n");
+    // TEMP Turn this on to help debug.
+    if (0) {
+      printf("File contents:'''\n");
+      array__for(char **, line, lines, i) printf("%s%s", (i ? "\n" : ""), *line);
+      printf("'''\n");
+    }
   }
 
   // Enter our read-eval-print loop (REPL).
   while (1) {
     char *line = readline("");
-    if (strcmp(line, "q") == 0) return 0;
+    run_command(line);
   }
 
   return 0;
